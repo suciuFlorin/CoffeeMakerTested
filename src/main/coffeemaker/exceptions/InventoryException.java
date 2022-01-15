1ac5f6ccf8aa1824834d1a28fb655c64b14e623e
suciuFlorin <flotzone2012@gmail.com>
2022-01-15 15:36:09 +0200

    Moved files to have shorter paths

---
+++ src/main/coffeemaker/exceptions/InventoryException.java
@@ -0,0 +1,11 @@
+package main.coffeemaker.exceptions;
+
+public class InventoryException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public InventoryException(String msg) {
+		super(msg);
+	}
+
+}
